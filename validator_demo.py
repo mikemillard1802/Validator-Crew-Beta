87bd3f884a812e72791ef7768a18c20d1363de29
mikemillard1802 <64797791+mikemillard1802@users.noreply.github.com>
2026-01-13 19:53:32 -0600

    Update validator_demo.py
    Code correction

--- validator_demo.py
+++ validator_demo.py
@@ -71,11 +71,10 @@ if st.button("Validate Idea"):
             )
 
             crew = Crew(agents=[researcher, analyst, writer], tasks=[task1, task2, task3], 
-            # If using planning, you MUST specify the planning_llm
+            # If using planning, you MUST specify the planning_llm,
             # planning=True, 
             # planning_llm=your_groq_llm, 
             memory=False  # Disable memory if you don't have an embedder set up
-)
             verbose=False)
             result = crew.kickoff()
 
